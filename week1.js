c891ad3d8ad95a152fd0bc562b9d2a8ea6410dbc
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-09-23 17:47:01 +0900

    [REFACTOR] 여러 음식 작동 성공 - 입력 한 번만 가능

--- week1.js
+++ week1.js
@@ -10,44 +10,87 @@ const emitter = new EventEmitter();
 
 const foodList = [];
 
-function calTime (what, size) {
-  let time = 0;
-  if(size==='소형')time = 1000;
-  else if(size==='중형')time=2000;
-  else if(size === '대형')time=3000;
-  return time;
-}
+// function calTime (what, size) {
+//   let time = 0;
+//   if(size==='소형')time = 1000;
+//   else if(size==='중형')time=2000;
+//   else if(size === '대형')time=3000;
+//   return time;
+// }
 
-let cookingIdx = -1;
+let cookingIdx = 0;
 let servingIdx = -1;
 
-const cook = (what, size) => {
-  const time = calTime(what, size);
-  console.log(`사이즈 : ${size}, 시간 : ${time}`);
-  console.log(`size는${size}`);
+const processing = () => {
   let seconds = 0;
+  let cookingTime = -1;
+  let servingTime = 0;
+  let servingAmounts = 0;
+  const size = foodList.length;
   const interval = setInterval(()=>{
     seconds++;
+    cookingTime++;
+    
     console.log(`${seconds}초`);
-    console.log(`${what} (${size}) 요리중`);
+
+    if(servingAmounts > 0){
+      servingTime++;
+    }
+    if(servingIdx>=0){
+      if(servingAmounts > 0 && servingTime == foodList[servingIdx][1]){
+        servingAmounts--;
+        servingTime = 0;
+      }
+    }
+
+    for(let i = 0; i<size;i++){
+
+      if(cookingTime==foodList[i][1]){ // 요리 다 되면
+        cookingTime = 0;
+        servingAmounts++;
+        if(servingAmounts == 1)
+          servingIdx++;
+        cookingIdx++;
+      }      
+      const what = foodList[i][0];
+      const size = makeFoodSize(foodList[i]);
+      if(i == cookingIdx){
+        console.log(`${what} (${size}) 요리중`);
+      }
+      else if(i == servingIdx){
+        console.log(`${what} (${size}) 서빙중`);
+      }
+      else if(i<servingIdx){
+        console.log(`${what} (${size}) 끝`);
+      }
+      else if(i > servingIdx){
+        console.log(`${what} (${size}) 대기중`);
+      }
+      
+    }
+    if(servingIdx == size){
+      finish(interval);
+    }
   }, 1000);
-  setTimeout(()=>{
-    clearInterval(interval);
-    serve(what, size);
-  }, time+1000);
 }
-const serve = (what, size) => {
-  const time = calTime(what, size);
-  let seconds = parseInt(time) / 1000;
-  const interval = setInterval(()=>{
-    seconds++;
-    console.log(`${seconds}초`);
-    console.log(`${what} (${size}) 서빙중`);
-  }, 1000);
-  setTimeout(()=>{
-    clearInterval(interval);
-    console.log('over');
-  }, time+1000);
+
+function makeFoodSize (food) {
+  const what = food[0];
+  const num = food[1];
+  if(num === '1'){
+    return size = '소형';
+  }
+  else if(num === '2'){
+    return size = '중형';
+  }
+  else if(num === '3'){
+    return size = '대형';
+  }
+}
+
+const finish = (interval) => {
+  clearInterval(interval);
+  console.log('오늘 샷다 내려!');
 }
 
 rl.question("원하는 음식을 입력하시오(예 - '스파게티, 3:1') : ", (line)=>{
@@ -56,8 +99,8 @@ rl.question("원하는 음식을 입력하시오(예 - '스파게티, 3:1') : ",
   for(let i=0;i<num;i++){
     foodList.push([foodName, what]);
   }
-  console.log(foodList);
-  emitter.emit('cooking', foodList.shift());
+  // console.log(foodList); // 음식 리스트 출력
+  emitter.emit('cooking', );
 })
 
 rl.on('line', (line)=>{
@@ -65,31 +108,14 @@ rl.on('line', (line)=>{
 
   const [foodName, foodInfo] = line.split(',').map(part => part.trim());
   const [what, num] = foodInfo.split(':');
-
-  for(let i=0;i<num;i++){
+  for(let i=0;i<num;i++){ 
     foodList.push({foodName:what});
     console.log(foodName, what);
   }
-  // process.exit();
 });
 
-emitter.on('cooking', (food)=>{
-  let size = '';
-  console.log(food);
-  // const [what, num] = food.split(':');
-  const what = food[0];
-  const num = food[1];
-  if(num === '1'){
-    size = '소형';
-  }
-  else if(num === '2'){
-    size = '중형';
-  }
-  else if(num === '3'){
-    size = '대형';
-  }
-  console.log(`cooking : ${size}`);
-  cook(what, size);
+emitter.on('cooking', ()=>{ 
+  processing();
 })
 
 
